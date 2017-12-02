723703fd844677c7bccebab75591b467a86dc2f7
SineLaude <34171186+SineLaude@users.noreply.github.com>
2017-12-03 00:04:19 +0100

    add files

---
+++ daten.js
@@ -0,0 +1,46 @@
+
+institutionen = []
+institutionen[institutionen.length] = ['Infos Neumitglieder',	0]
+// institutionen[institutionen.length] = ['F�r Einsteiger',						'index.php3?position=450']
+institutionen[institutionen.length] = ['Tutor-Ini',						'index.php3?position=14000&ini_id=545']
+institutionen[institutionen.length] = ['Neumitglieder-Ini',						'index.php3?position=14000&ini_id=4217']
+institutionen[institutionen.length] = ['Neue Mitglieder',				'javascript:open_std_window(\'/statistik/neue_mitglieder.php\',400,580)']
+institutionen[institutionen.length] = ['Dol-Tour',						'index.php3?position=100&tourId=InfoTourDol2day&skinId=dol2day']
+
+institutionen[institutionen.length] = ['Initiativen-Suche',				0]
+institutionen[institutionen.length] = ['Ini-&Uuml;bersicht',			'javascript:open_std_window(\'/initiativen/alle_initiativen.html\',875,520)']
+
+institutionen[institutionen.length] = ['Regierung',						0]
+institutionen[institutionen.length] = ['Kanzleramt',					'index.php3?position=11000']
+institutionen[institutionen.length] = ['Agora (Kanzleramtsforum) ',			'index.php3?position=11100&ini_id=4242']
+// institutionen[institutionen.length] = ['Regierungs-ToDo-Liste',				'index.php3?position=23000']
+// institutionen[institutionen.length] = ['Dol-Blog',						'http://www.dol2day.com/blog']
+
+institutionen[institutionen.length] = ['dol2day-Verein',						0]
+institutionen[institutionen.length] = ['dol2day e.V.',					'index.php3?position=14000&ini_id=1423']
+
+institutionen[institutionen.length] = ['dol2day Institutionen',			0]
+institutionen[institutionen.length] = ['Politisches Duell',				'index.php3?position=25100']
+institutionen[institutionen.length] = ['dol2day-Gesetzbuch',			'index.php3?position=8500']
+institutionen[institutionen.length] = ['Schiedsgericht',				'index.php3?position=8400']
+institutionen[institutionen.length] = ['Standesamt',					'index.php3?position=13000']
+institutionen[institutionen.length] = ['Parteigr&uuml;ndungen',			'index.php3?position=6200']
+institutionen[institutionen.length] = ['Parteimitgliederzahlen',		'index.php3?position=6300']
+institutionen[institutionen.length] = ['Abgelehnte Umfragen',			'index.php3?position=4070']
+institutionen[institutionen.length] = ['Volksinitiative',				'index.php3?position=14000&ini_id=8']
+
+institutionen[institutionen.length] = ['Redaktion',				0]
+institutionen[institutionen.length] = ['Dol-Profile',						'index.php3?position=14000&ini_id=4022']
+// institutionen[institutionen.length] = ['Redax-ToDo-Liste',				'index.php3?position=20000']
+// institutionen[institutionen.length] = ['Technik-Vorschl�ge',			'index.php3?position=2205&typ=ini_id&typ_id=3727&forum_id=6848']
+// institutionen[institutionen.length] = ['Fehlermeldungen',				'index.php3?position=2205&typ=ini_id&typ_id=3727&forum_id=6850']
+// institutionen[institutionen.length] = ['Offenes Redax-Forum',			'index.php3?position=2205&typ=ini_id&typ_id=2517&forum_id=5740']
+
+institutionen[institutionen.length] = ['Real Life (RL)',				0]
+institutionen[institutionen.length] = ['Termine RL-Treffen',			'javascript:open_std_window(\'/rl_treffen.php\',350,500)']
+// institutionen[institutionen.length] = ['Photos von RL-Treffen',			'javascript:open_std_window(\'dolphotos/index.php\',800,480)']
+
+institutionen[institutionen.length] = ['Sonstiges',						0]
+institutionen[institutionen.length] = ['Dol-Statistiken',				'index.php3?position=600']
+institutionen[institutionen.length] = ['Dol-Monitor',				'index.php3?position=710']
+institutionen[institutionen.length] = ['Dol-Rezension',				'index.php3?position=4005']
